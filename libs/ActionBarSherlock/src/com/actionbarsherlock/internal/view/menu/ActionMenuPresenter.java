116c65df90a4307a8460b6fe49dcf24b9eaffc26
Morgan Gangwere <indrora@earfolds.com>
2014-04-02 02:22:16 -0600

    Famangled ABS into letting me show the menu always.
    ABS now always thinks there's no hardware button. I also broke something
    somehow last commit (I didn't stash right)

--- libs/ActionBarSherlock/src/com/actionbarsherlock/internal/view/menu/ActionMenuPresenter.java
+++ libs/ActionBarSherlock/src/com/actionbarsherlock/internal/view/menu/ActionMenuPresenter.java
@@ -118,16 +118,12 @@ public class ActionMenuPresenter extends BaseMenuPresenter
     }
 
     public static boolean reserveOverflow(Context context) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB);
-        } else {
-            return !HasPermanentMenuKey.get(context);
-        }
+    	return true;
     }
 
     private static class HasPermanentMenuKey {
         public static boolean get(Context context) {
-            return ViewConfiguration.get(context).hasPermanentMenuKey();
+            return false;
         }
     }
 
