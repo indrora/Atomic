ba393d55dbbfd96f6c525f3d3a0e9249b22eab18
Morgan Gangwere <indrora@earfolds.com>
2014-04-22 11:20:16 -0600

    There will now be a central Application object to handle some things later on. Woo!

---
+++ application/src/indrora/atomic/App.java
@@ -0,0 +1,15 @@
+package indrora.atomic;
+
+import indrora.atomic.model.ColorScheme;
+import android.app.Application;
+import android.util.Log;
+
+public class App extends Application {
+
+	Atomic atomic;
+	public App()
+	{
+		super();
+	}
+	
+}
